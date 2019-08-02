60ead5796aa2da3bf9526701964645c3bb1e0342
maraya12 <mariateresacumayas@gmail.com>
2019-08-02 19:56:55 +0800

    update

--- javascript functions/script.js
+++ javascript functions/script.js
@@ -1,28 +1,49 @@
-//object
+// get price value
+function getProduct(){
+  return document.getElementById('product').value;
+}
 
 // get price value
-function price(){
-     return Number(document.getElementById('price').value);
+function getPrice(){
+  return document.getElementById('price').value;
 }
 
 // get quantity
-function quantity(){
-    return Number(document.getElementById('quantity').value);
-}
-
-
-// get total
-function getTotal(){
-  document.getElementById('total').innerHTML = "Total: " + (quantity() * price())
+function getQuantity(){
+  return document.getElementById('quantity').value;
 }
 
 // get cash
-// function cash(){
-//    return Number(document.getElementById('cash').value);
-//}
+function getCash(){
+  return document.getElementById('cash').value;
+}
 
-//display summary
+//button functions
+// get purchase total
 
+function getTotal(){
+  if(getProduct() == "" || getPrice() == "" || getQuantity() ==""){
+    $(".msg1").text("Error. Please provide missing info.");
+  }
+  else if(isNaN(getPrice()) || isNaN(getQuantity())){
+    $(".msg2").text("Error. Please input a number.");
+  }
+  else{
+    return document.getElementById('result1').innerHTML = getPrice() * getQuantity();
+  }
+}
 
+// save to display summary
 
-
+function save(){
+  if(getTotal() <= getCash()){
+    document.getElementById("SumProduct").innerHTML = getProduct();
+    document.getElementById("SumPrice").innerHTML = getPrice();
+    document.getElementById("SumQty").innerHTML = getQuantity();
+    document.getElementById("SumTotal").innerHTML = getTotal();
+    document.getElementById("SumCash").innerHTML = getCash();
+    document.getElementById("SumChange").innerHTML = getCash() - getTotal();
+  }else{
+    $(".msg3").text("Insufficient Cash!")
+  }
+}
